b83c17dfbcc9d4b58064116601ace8118ffec39f
Payayaing <isabella.watt22@gmail.com>
2025-08-24 20:53:15 +1000

    Another test to make sure the repository is working correctly.

--- 40005-AssessmentTask2/40005-AssessmentTask2/ContentView.swift
+++ 40005-AssessmentTask2/40005-AssessmentTask2/ContentView.swift
@@ -10,10 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "star.fill")
+            Text("This is a test!")
         }
         .padding()
     }
