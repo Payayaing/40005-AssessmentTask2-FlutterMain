ded5fc1ff4cc414999a77b34d038949ab899731f
Payayaing <isabella.watt22@gmail.com>
2025-08-24 20:47:33 +1000

    Added the initial XCode file that will be built upon for this assessment. Also will be used to make sure the repository is syncing correctly.

---
+++ 40005-AssessmentTask2/40005-AssessmentTask2/_0005_AssessmentTask2App.swift
@@ -0,0 +1,17 @@
+//
+//  _0005_AssessmentTask2App.swift
+//  40005-AssessmentTask2
+//
+//  Created by Bella on 24/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct _0005_AssessmentTask2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 40005-AssessmentTask2/40005-AssessmentTask2Tests/_0005_AssessmentTask2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  _0005_AssessmentTask2Tests.swift
+//  40005-AssessmentTask2Tests
+//
+//  Created by Bella on 24/8/2025.
+//
+
+import Testing
+@testable import _0005_AssessmentTask2
+
+struct _0005_AssessmentTask2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
